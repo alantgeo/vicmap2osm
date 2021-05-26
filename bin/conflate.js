e02bbf8c911081e2d31d3768decfd9b52819d48f
Andrew Harvey <andrew@alantgeo.com.au>
2021-05-26 13:24:09 +1000

    addr:street must be present to match

--- bin/conflate.js
+++ bin/conflate.js
@@ -179,7 +179,8 @@ const conflate = new Transform({
             // ignoring case
             // ignoring unit for the time being
             // ignoring differences between "Foo - Bar Street" and "Foo-Bar Street", these kinds of names are common in country victoria
-            return (feature.properties['addr:street'].toLowerCase().replaceAll(' - ', '-') === osmStreet.toLowerCase().replaceAll(' - ', '-'))
+            return feature.properties['addr:street'] !== null && osmStreet !== null
+              && feature.properties['addr:street'].toLowerCase().replaceAll(' - ', '-') === osmStreet.toLowerCase().replaceAll(' - ', '-')
               && osmHouseNumber !== null && feature.properties['addr:housenumber'].replaceAll(' ', '').toLowerCase() === osmHouseNumber.replaceAll(' ', '').toLowerCase()
               && (('addr:unit' in feature.properties && osmUnit !== null) ? feature.properties['addr:unit'].replaceAll(' ', '') === osmUnit.replaceAll(' ', '') : true)
           })
